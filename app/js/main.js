5dfbba554f9757c75249c0b207dc15abd0a7586f
SandoHTML <verstkatw@gmail.com>
2021-10-06 22:28:26 +0300

    lesson13

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,30 @@
 $(function () {
 
-    $('.header__slider').slick({
+    $('.slider-discount__list').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        prevArrow: $('.slider-discount__arrow--prev'),
+        nextArrow: $('.slider-discount__arrow--next')
       });
-      
 
-      var mixer = mixitup('.products__content');
+
+
+
+
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+
+  var config = {
+      controls: {
+        scope: 'local'
+      }
+    };
+
+ 
+    var mixer = mixitup(containerEl1, config);
+    var mixer = mixitup(containerEl2, config);
+
   });
